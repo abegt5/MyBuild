f158959635a09b3630bad3c77c520792d55206e3
Abegt5 <abegt5@gmail.com>
2025-02-20 03:18:09 -0800

    working

--- app/tabs/home.tsx
+++ app/tabs/home.tsx
@@ -6,7 +6,7 @@ export default function HomeScreen() {
 
     return (
         <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
-            <Text>📰 Home Feed</Text>
+            <Text>📰 Home Feedddd</Text>
             <Button title="Go to Add Post" onPress={() => router.push("/addPost")} />
         </View>
     );
